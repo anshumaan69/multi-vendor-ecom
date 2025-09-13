8dea2f429883e29ba36b868793ddb2a9a443287c
Anshumaan <anshumaan.1611@gmail.com>
2025-09-13 16:23:02 +0530

    customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,46 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+import { Progress } from "@/components/ui/progress";
+
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
 
 
 export default function Home() {
   return (
-    <div>
-      <div className="text-rose-600">
-        Hello World
+
+      <div className="flex flex-col gap-y-4  p-5 ">
+        <div className="text-rose-600">
+          Hello World\
+
+
+
+        </div>
+        <div>
+                  <Button variant="elevated">
+            have Sex
+          </Button>
+          </div>
+
+        <Input placeholder="I am the inout ">
+        </Input>
+        <div>
+
+          <Progress value = {50}></Progress>
+
+        </div>
+
+        <div>
+          <Textarea   value ="I am text area"></Textarea>
+        </div>
+
+
+        <div>
+          <Checkbox  value ="I am text area"></Checkbox>
+        </div>
+
 
       </div>
-
-
-    </div>
       );
 }
